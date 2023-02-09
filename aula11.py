c583d45b7086d5bed5d2e2fbbcdb8833fb1f1f61
André Loureço Marques <andrelm3009@gmail.com>
2023-02-09 11:17:09 -0300

    aula11.py

---
+++ aula11.py
@@ -0,0 +1,10 @@
+# print('\033[0;35;40mOlá mundo!')
+# print('\033[7;35;40mOlá mundo!')
+
+# a = 5
+# b = 6
+# print('\033[mOs valores são \033[1;35m{}\033[m e \033[1;34m{}'.format(a,b))
+
+# nome= 'André'
+# cores={'Limpa':'\033[m','Azul':'\033[36m','PretoeBranco':'\033[7;37m'}
+# print('\033[mOlá {}{}{}, prazer em te conhecer!'.format(cores['PretoeBranco'],nome,cores['Limpa']))
