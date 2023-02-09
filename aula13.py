cb8c835bb320c5e9ce8c837b0a80065757c17089
André Loureço Marques <andrelm3009@gmail.com>
2023-02-09 11:21:37 -0300

    aula13.py

---
+++ aula13.py
@@ -0,0 +1,30 @@
+#Desafio 46
+# import time
+# print('Contagem para estourar os fogos!')
+# for c in range(10, -1, -1 ):
+#     print(c)
+#     time.sleep(1)
+# print('BOOOMMMM!')
+
+#Desafio 47
+# import time
+# for c in range(1, 51):
+#     if c % 2 == 0:
+#         print(c)
+#         time.sleep(0.5)
+# print('==================')
+
+#Desafio 48
+# soma = 0
+# cont = 0
+# for c in range(1, 501, 2):
+#     if c % 3 == 0:
+#         soma = soma + c
+#         cont = cont + 1
+# print('A soma dos {} números multiplos de 3 é: {}'.format(cont, soma))
+
+#Desafio 49
+# num= int(input('Qual tabuada voce quer ver? '))
+# for c in range (0, 11):
+#     mult= c * num
+#     print('{} x {} = {}'.format(num, c, mult))
