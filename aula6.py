3ccc5c67ec09ca13a1780fa822909bd15e788fee
André Loureço Marques <andrelm3009@gmail.com>
2023-02-09 11:10:57 -0300

    aula6.py

---
+++ aula6.py
@@ -0,0 +1,11 @@
+n1= input('Digite algo: ')
+print( 'O valor digitado eh  um numero?  ')
+print (n1.isnumeric())
+
+print( 'O valor digitado eh  um caracter?   ' )
+print(n1.isalpha())
+
+print( 'O valor digitado eh  um numero com caracter?   ')
+print(n1.isalnum())
+
+
