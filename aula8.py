b5f248c5a0cf58b6562cfe6577e9ceb1939a7972
André Loureço Marques <andrelm3009@gmail.com>
2023-02-09 11:13:32 -0300

    aula8.py

---
+++ aula8.py
@@ -0,0 +1,30 @@
+#Desafio 16
+#from math import trunc
+#num=float(input('Digite um valor: '))
+#inteiro= trunc(num)
+#print('A parte inteira de {}, e {}.'.format(num, inteiro))
+
+#Desafio 17
+#from math import sqrt,pow
+#co=float(input('Cateto Oposto: '))
+#ca=float(input('Cateto Adjacente: '))
+#hp= sqrt(pow(co,2)+pow(ca,2))
+#print('A hipotenusa do triangulo eh {}'.format(hp))
+
+#Desafio 18
+#import math
+#ang= float(input('Digite um angulo: '))
+#cosseno= math.cos(math.radians(ang)) 
+#seno= math.sin(math.radians(ang)) 
+#tang= (math.tan(math.radians(ang))) 
+#print('Seno: {:.2f} \nCosseno: {:.2f} \nTangente: {:.2f}'.format(seno, cosseno, tang))
+
+#Desafio 19
+#import random
+#a1= str(input('Primeiro aluno: '))
+#a2= str(input('Segundo aluno: '))
+#a3= str(input('Terceiro aluno: '))
+#lista=[a1, a2, a3]
+#escolha= random.choice(lista)
+#print('O aluno (a) escolhido foi {}'.format(escolha))
+
