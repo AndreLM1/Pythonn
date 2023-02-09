22f519f2599bf89092c90336dc008bfd539f29a8
André Loureço Marques <andrelm3009@gmail.com>
2023-02-09 11:12:24 -0300

    aula7.py

---
+++ aula7.py
@@ -0,0 +1,41 @@
+#Desafio 5
+#n1=int((input('Digite um numero ')))
+#an = n1 - 1
+#dp = n1+1
+#print(' O numero antecessor de {}, eh {}, e o seu sucessor eh {}' .format(n1, an, dp ))
+
+#Desafio 6
+#n= int(input('Digite um numero ') )
+#d = n * 2
+#t = n * 3
+#sqr = n**(1/2)
+#print('Dobro: {}\ntriplo: {}\nraiz quadr25ada: {}'.format(d, t , sqr))
+
+#Desafio 7
+#n1= float(input('Nota 1: '))
+#n2= float(input('Nota 2: '))
+#med = (n1 + n2) / 2
+#print('Sua media eh: {}'.format(med))
+
+#Desafio 8 
+#m=float(input('valor em metros: '))
+#cm = m * 100
+#mm = m * 1000
+#print('Metros: {}\nCentimetros: {}\nMilimetros: {}'.format(m, cm, mm))
+
+#Desafio 11
+#l=float(input('Largura da parede: '))
+#a= float(input('Altura da parede: '))
+#area= a* l
+#print('Area da parede: {} m^2'.format(area))
+#print('Voce precisa de {} litros de tinta'.format(area/2))
+
+#Desafio 12
+#val= float(input('Preco do produto: '))
+#desc = (val * 0.95)
+#print('Com 5% de desconto, o produto custara  R${:.2f} '.format(desc))
+
+#Desafio 13
+#sal= float(input('Salario do funcionario: R$ '))
+#aumento= (sal * 1.15)
+#print('Com aumento de 15%, seu novo salario sera: R${:.2f}'.format(aumento))
