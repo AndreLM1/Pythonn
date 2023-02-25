301be6c28a850d036c8a2a1deb2bf3b028c0871b
André Loureço Marques <andrelm3009@gmail.com>
2023-02-25 00:16:14 -0300

    ate o desafio 67

---
+++ aula15.py
@@ -0,0 +1,23 @@
+#Desafio 66
+# n= soma= cont= 0
+# print('Caso queira sair do programa, digite 999')
+# while True:
+#     n=int(input('Digite um valor: '))
+#     if n == 999:
+#         break
+#     soma += n
+#     cont += 1
+# print('A soma dos {} números vale {}'.format(cont, soma))
+
+#Desafio 67
+# while True:
+#     num= int(input('Qual tabuda você quer ver? '))
+#     if num < 0:
+#         break
+#     print(10*'-')
+#     for c in range (0,11): 
+#         mult= num * c
+#         print('{} x {} = {}'.format(num, c, mult))    
+#     print(10*'-')
+# print('Programa encerrado!')  
+    
